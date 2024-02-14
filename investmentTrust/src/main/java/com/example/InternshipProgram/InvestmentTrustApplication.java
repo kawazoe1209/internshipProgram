cb5991e837bf21833367ce2df2154f8826583b5e
ffg-training <internship0216to0222@gmail.com>
2024-02-14 10:43:45 +0900

    　リファクタ

--- investmentTrust/src/main/java/com/example/InternshipProgram/InvestmentTrustApplication.java
+++ investmentTrust/src/main/java/com/example/InternshipProgram/InvestmentTrustApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InternshipProgramApplication {
+public class InvestmentTrustApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InternshipProgramApplication.class, args);
+		SpringApplication.run(InvestmentTrustApplication.class, args);
 	}
 
 }
---
+++ investmentTrust/src/test/java/com/example/InternshipProgram/InvestmentTrustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InternshipProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentTrustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
