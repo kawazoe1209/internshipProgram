614d9ef60a54c286a9e7fecfe0da7a21104fa3e6
internship0216to0222 <internship0216to0222@gmail.com>
2024-02-15 12:16:07 +0900

    エラー修正（ゲッターが使えなくなってた突如）

--- bankTransfer/src/main/java/com/example/InternshipProgram/bankTransfer/BankTransferApplicationForm.java
+++ bankTransfer/src/main/java/com/example/InternshipProgram/bankTransfer/BankTransferApplicationForm.java
@@ -9,4 +9,28 @@ public class BankTransferApplicationForm {
     private String bankName;
     private Integer bankAccountNum;
     private Integer money;
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankName(){
+        return this.bankName;
+    }
+
+    public void setBankAccountNum(Integer bankAccountNum) {
+        this.bankAccountNum = bankAccountNum;
+    }
+
+    public Integer getBankAccountNum(){
+        return this.bankAccountNum;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    public Integer getMoney(){
+        return this.money;
+    }
 }
--- investmentTrust/src/main/java/com/example/InternshipProgram/investmentTrust/InvestmentTrustApplicationForm.java
+++ investmentTrust/src/main/java/com/example/InternshipProgram/investmentTrust/InvestmentTrustApplicationForm.java
@@ -10,4 +10,36 @@ public class InvestmentTrustApplicationForm {
     private String bankName;
     private Integer bankAccountNum;
     private Integer money;
+
+    public void setFundName(String fundName) {
+        this.fundName = fundName;
+    }
+
+    public String getFundName() {
+        return this.fundName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getBankName(){
+        return this.bankName;
+    }
+
+    public void setBankAccountNum(Integer bankAccountNum) {
+        this.bankAccountNum = bankAccountNum;
+    }
+
+    public Integer getBankAccountNum(){
+        return this.bankAccountNum;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    public Integer getMoney(){
+        return this.money;
+    }
 }
