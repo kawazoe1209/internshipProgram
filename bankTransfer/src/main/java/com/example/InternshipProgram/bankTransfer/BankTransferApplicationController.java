518f9a2aef71392ff70e5f28bd651a118adf339a
ffg-training <internship0216to0222@gmail.com>
2024-02-21 10:35:25 +0900

    投信銘柄選択ボタン追加

--- bankTransfer/src/main/java/com/example/InternshipProgram/bankTransfer/BankTransferApplicationController.java
+++ bankTransfer/src/main/java/com/example/InternshipProgram/bankTransfer/BankTransferApplicationController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BankTransferApplicationController {
@@ -18,8 +20,23 @@ public class BankTransferApplicationController {
     @GetMapping("/bankTransfer")
     public String bankTransfer(Model model) {
         model.addAttribute("bankTransferApplication", new BankTransferApplicationForm());
-        model.addAttribute("nameOptions", "福岡");
+        Map<Integer, String> nameOptions = new HashMap<>();
+        nameOptions.put(1, "福岡");
+        nameOptions.put(2, "S");
+        nameOptions.put(3, "N");
+        model.addAttribute("nameOptions",nameOptions);
+        Map<Integer, String> nameOptions1 = new HashMap<>();
+        nameOptions1.put(1, "普通");
+        nameOptions1.put(2, "当座");
+        nameOptions1.put(3, "定期");
+        nameOptions1.put(4, "貯蓄");
+        nameOptions1.put(5, "外貨");
+        nameOptions1.put(6, "通知");
+        nameOptions1.put(7, "別段");
+        nameOptions1.put(8, "納税準備");
+        model.addAttribute("nameOptions1",nameOptions1);
         return "bankTransfer/bankTransferMain";
+
     }
 
     @PostMapping("/bankTransferConfirmation")
